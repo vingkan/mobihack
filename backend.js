b64a647ff290e94cfc11d41d1f144823f038a583
Recursions <Nick@dhcp25.merusouth113.iit.edu>
2015-11-14 05:17:22 -0600

    notify is half working

--- backend.js
+++ backend.js
@@ -1,21 +1,34 @@
 var firebase = new Firebase("https://claritk.firebaseio.com/");
 var firebaseRooms = firebase.child("Rooms");
+var listening;
+
+function addDevice(room, userIp) {
+	var newDevice = firebaseRooms.child(room+"/"+userIp);
+	newDevice.push(0);
+}
+
 
 function setListening(room, bool) {
 	var newListener = firebaseRooms.child(room+"/LISTENING");
 	newListener.set(bool);
 }
 
-function notifyDevices(room) {
-	var listenDir = firebaseRooms.child(room+"/LISTENING");
-	listenDir.on("child_changed", function(snapshot){
-		console.log("entered child changed");
-		var changedPost = snapshot.val();
-		console.log("Listening updated: "+ changedPost);
-	});
+firebaseRooms.child("roomkey/LISTENING").on("value", notify);
+
+function notify(snapshot) {
+	console.log(snapshot.key());
 }
 
-function addDevice(room, userIp) {
-	var newDevice = firebaseRooms.child(room+"/"+userIp);
-	newDevice.push(0);
-}
+// function notifyDevices(room) {
+// 	var listenDir = firebaseRooms.child(room+"/LISTENING");
+// 	console.log("Getting child changed status");
+// 	listenDir.on("child_changed", function(snapshot){
+// 		console.log("entered child changed");
+// 		var changedPost = snapshot.val();
+// 		console.log("Listening updated: "+ changedPost);
+// 	});
+// }
+
+
+
+
