5c109653d00387e2f0655db5124acfa72a3e5380
Vinesh Kannan <vingkan@gmail.com>
2015-11-14 03:50:28 -0600

    Set up recursion

--- script/application.js
+++ script/application.js
@@ -6,6 +6,20 @@ var ROOM_KEY;
 var DEVICE_KEY;
 
 var currentResult = null;
+var parsedData = {
+	transcript: "transcript",
+	confidence: 0.5
+};
+
+var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event){
+	currentResult = event;
+	console.log("Returned Result:");
+	console.log(currentResult);
+	console.log(getResultTranscript());
+	console.log(getResultConfidence());
+	parseResult();
+}
 
 /*--------------------------------------------*/
 /*---> STEP ONE <-----------------------------*/
@@ -23,6 +37,21 @@ function startListening(){
 	
 }
 
+/*--------------------------------------------*/
+/*---> STEP THREE <---------------------------*/
+/*--------------------------------------------*/
+
+function record(){
+	recognition.start();
+}
+
+function parseResult(){
+	parsedData.transcript = getResultTranscript();
+	parsedData.confidence = getResultConfidence();
+	pushResult(parsedData);
+	record();
+}
+
 
 function getResultTranscript(){
 	return currentResult.results[0][0]['transcript'];
@@ -32,16 +61,6 @@ function getResultConfidence(){
 	return currentResult.results[0][0]['confidence'];
 }
 
-var recognition = new webkitSpeechRecognition();
-recognition.onresult = function(event){
-	currentResult = event;
-	console.log("Returned Result:");
-	console.log(currentResult);
-	console.log(getResultTranscript());
-	console.log(getResultConfidence());
-}
-recognition.start();
-
 console.log('LOADED APPLICATION');
 
 /*--------------------------------------------*/
@@ -50,6 +69,11 @@ console.log('LOADED APPLICATION');
 
 function addDevice(roomKey){
 	return 'device-key1';
+}
+
+function pushResult(resultData){
+	console.log("Sent to Database:");
+	console.log(resultData);
 }
 
 console.log('LOADED SUBSTITUTES');
