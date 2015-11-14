8ef04b6c581bf4fbfdccb0240757de13238df8f2
Vinesh Kannan <vingkan@gmail.com>
2015-11-14 03:36:25 -0600

    Get results from speechWebkit and print to the console!

--- script/application.js
+++ script/application.js
@@ -1,1 +1,21 @@
+var currentResult = null;
+
+function getResultTranscript(){
+	return currentResult.results[0][0]['transcript'];
+}
+
+function getResultConfidence(){
+	return currentResult.results[0][0]['confidence'];
+}
+
+var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event){
+	currentResult = event;
+	console.log("Returned Result:");
+	console.log(currentResult);
+	console.log(getResultTranscript());
+	console.log(getResultConfidence());
+}
+recognition.start();
+
 console.log('LOADED APPLICATION');
