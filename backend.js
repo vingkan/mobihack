0434edf66c0d7620c7c7627b80763d06295709d0
Recursions <Nick@dhcp25.merusouth113.iit.edu>
2015-11-14 04:57:33 -0600

    Working with notifying devices

--- backend.js
+++ backend.js
@@ -1,5 +1,21 @@
 var firebase = new Firebase("https://claritk.firebaseio.com/");
+var firebaseRooms = firebase.child("Rooms");
 
-function addDevice(room) {
-	var roomLocator = firebase.child(room);
+function setListening(room, bool) {
+	var newListener = firebaseRooms.child(room+"/LISTENING");
+	newListener.set(bool);
+}
+
+function notifyDevices(room) {
+	var listenDir = firebaseRooms.child(room+"/LISTENING");
+	listenDir.on("child_changed", function(snapshot){
+		console.log("entered child changed");
+		var changedPost = snapshot.val();
+		console.log("Listening updated: "+ changedPost);
+	});
+}
+
+function addDevice(room, userIp) {
+	var newDevice = firebaseRooms.child(room+"/"+userIp);
+	newDevice.push(0);
 }
