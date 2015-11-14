2d0b8c2b93787b2678e5fa8b389a07a3aa41dafb
mohsinhaider <mohsinshaider@gmail.com>
2015-11-14 05:06:58 -0600

    Very miniscule changes

--- script/frontend.js
+++ script/frontend.js
@@ -1,1 +1,21 @@
 console.log('LOADED FRONTEND');
+// global variable room key
+
+var ROOM_KEY;
+
+function setRoomKey()
+{
+	ROOM_KEY = document.getElementById("ROOM_KEY").value;
+	joinRoom(ROOM_KEY); // how do we know this even exists
+}
+
+function printResults(device)
+{
+
+	/**
+	var html = '';
+	html += '<div id="' + this.id + '" class="tower preset" onclick="setFocusObject(\'towers\', \'' + this.id + '\');">' + this.size + '</div>';
+	return html;
+	**/ // Same thing from Troops.js from conquiz??
+	// somehow call resultToHTML
+}
