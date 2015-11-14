4b550f74008d953dfea13aabe81ef40870d0c3cc
Vinesh Kannan <vingkan@gmail.com>
2015-11-14 04:32:55 -0600

    Fetch user IP address for unique device key name

--- script/application.js
+++ script/application.js
@@ -17,8 +17,32 @@ var parsedData = {
 /*---> STEP ONE <-----------------------------*/
 /*--------------------------------------------*/
 
+/*
+* S/O Link: http://stackoverflow.com/questions/391979/get-client-ip-using-just-javascript
+*/
+function getDeviceIP() {
+	if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+	else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+	xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+	xmlhttp.send();
+
+	hostipInfo = xmlhttp.responseText.split("\n");
+
+	for (i=0; hostipInfo.length >= i; i++) {
+		ipAddress = hostipInfo[i].split(":");
+		if ( ipAddress[0] == "IP" ){
+			document.getElementById('userID').innerHTML = ipAddress[1];
+			return ipAddress[1];
+		}
+	}
+
+	return false;
+}
+
 function joinRoom(roomKey){
-	DEVICE_KEY = addDevice(roomKey);
+	var userIP = getDeviceIP();
+	DEVICE_KEY = addDevice(roomKey, userIP);
 }
 
 /*--------------------------------------------*/
